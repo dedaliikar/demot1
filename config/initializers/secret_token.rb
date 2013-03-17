55b399bfb457262b4678ca33306243963b3cb4b1
Szkolenie <szkolenie@szkolenie1.(none)>
2013-03-17 10:07:52 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kwejkodemot::Application.config.secret_token = 'fa45ee8d6f2a6a364fc558d99ac7991af4a1b4ef118b0b2354244930df466cc070135917b51b28a6e46899554d1ee1d0e36ceb4f1875eed9512f6ed25fdf8fc0'
